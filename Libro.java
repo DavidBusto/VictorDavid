5ec8d2b100b16148af89c37f42aef0e57f71e8de
david busto sanchez <david.busto.2002@gmail.com>
2023-03-13 12:34:47 +0100

    versionLibro3

--- Libro.java
+++ Libro.java
@@ -44,6 +44,7 @@ public class Libro {
 	public void devolver(){
 		System.out.println("No se puede prestar porque esta prestado");
 		System.out.println("aaaaaa");
+		System.out.println("bbbbbb");
 		if (!this.estaPrestado())
 			System.out.println("No se puede devolver porque no esta prestado");
 		else prestado=false;	}
